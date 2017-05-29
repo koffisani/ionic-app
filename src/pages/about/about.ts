2ee87e9001100515c09601d517aa3112f2792ec6
Koffi Sani <koffisani@gmail.com>
2017-05-30 00:42:49 +0100

    Ajout d'animations d'actualisation par rafraichissement de page

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -7,8 +7,40 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
 
+   public quotes = [
+    "Once you have their money, never give it back",
+    "You can't cheat an honest customer, but it never hurts to try",
+    "Never spend more for an acquisition than you have to",
+    "If you can't break a contract, bend it",
+    "Never let family stand in the way of opportunity",
+    "Always keep you ears open",
+    "Keep count of your change",
+    "Instinct plus opportunity equals profit",
+    "A dead customer can't buy as much as a live one",
+    "Latinum isn't the only thing that shines"
+  ]
+
+  public quote = "";
+
+  constructor(public navCtrl: NavController) {
+    this.quote = this.randomQuote();
+    console.log(this.quote);
   }
 
+  newQuote(refresher) {
+    //setTimeout idea from Ionic Docs!
+    setTimeout( () => {
+      this.quote = this.randomQuote();
+      refresher.complete();
+    }, 1000);
+  }
+
+  randomQuote() {
+    return this.quotes[this.getRandomInt(0, this.quotes.length)];
+  }
+
+  getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 }
