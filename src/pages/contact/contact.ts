ead2cbb2ac218cc8459e45ab2ebf9e53cc85f875
Koffi Sani <koffisani@gmail.com>
2017-05-30 02:16:16 +0100

    Create a new tab and added slides

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -7,8 +7,27 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController) {
+  public cats = [];
 
+  constructor(public navCtrl: NavController) {
+    //create some initial cats
+    for(let i=0; i < 25; i++) {
+      this.cats.push('Cat ' + i);
+    }
+  }
+/**
+ * Generates more cats to display as more as user 
+ * scrolls down 
+ * @param infiniteScroll event
+ */
+  moreCats(infiniteScroll) {
+    //setTimeout 
+    setTimeout(() => {
+      for(let i=0; i < 25; i++) {
+        this.cats.push('Cat ' + (this.cats.length + 1));
+      }
+      infiniteScroll.complete();
+    }, 1000);
   }
 
 }
---
+++ src/pages/other/other.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+    selector: 'page-other',
+    templateUrl: 'other.html'
+})
+export class OtherPage {
+    
+    constructor(public navCntrl: NavController) {
+
+    }
+}
+
